0aefae3f4330506974e54a515222d1398ff71508
Nestor Alonso <nalonso@student.42.fr>
2019-03-01 15:18:56 +0100

    fixed strequ()

--- libft/ft_strequ.c
+++ libft/ft_strequ.c
@@ -6,7 +6,7 @@
 /*   By: nalonso <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 16:50:38 by nalonso           #+#    #+#             */
-/*   Updated: 2019/02/22 11:54:05 by nalonso          ###   ########.fr       */
+/*   Updated: 2019/03/01 13:55:02 by nalonso          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,10 @@
 
 int	ft_strequ(char const *s1, char const *s2)
 {
-	if (!s1 || !s2)
-		return (0);
-	while (*s1 && *s2)
-	{
-		if (*s1 != *s2)
-			return (0);
-		s1++;
-		s2++;
-	}
-	if ((!*s1 && *s2) || (*s2 && !*s1))
-		return (0);
-	return (1);
+	size_t i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] == s2[i] ? 1 : 0);
 }
